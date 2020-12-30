9432ef460ce4dbf0eeb0525ce395d905e4a4d07b
Vaibhav Chopra <vaibhavchopra72@gmail.com>
2020-12-30 21:19:44 +0530

    Fixed some code for better UX

--- stone paper scissor easy level.py
+++ stone paper scissor easy level.py
@@ -22,13 +22,21 @@ def gamewinner(computer, you):
             return True
 
 
-playsound("my recordings\\start.mpeg")
 choice = 'y'
 
-print("\n\nFirst turn will be Of computer. You have to enter:\n's'for stone\n'p'for paper\n'sc' for scissor\n\nNOTE:- If you enter anything other than 's', 'sc' or 'p', then it's an ERROR & you gotta, try again!!")
+print('''\n\nFirst turn will be Of computer. Following is the menu of the game:
+
+
+                                's' for stone
+                                'p' for paper
+                                'sc' for scissor
+                        
+    
+NOTE:- If you enter anything other than 's', 'sc' or 'p', then it's an ERROR & you gotta, try again!''')
+
+playsound("my recordings\\start.mpeg")
 
 while choice == 'y' or choice == 'Y':
-
 
     random_no = random.randint(1, 3)
 
@@ -43,19 +51,23 @@ while choice == 'y' or choice == 'Y':
 
     user_Turn = input("\n\t\tEnter your choice: ")
 
+    if user_Turn != 's' and user_Turn != "sc" and user_Turn != 'p':
+        print("\nYou entered a wrong value. Please try again!\n")
+        continue
+
     function_returning = gamewinner(comp, user_Turn)
 
     print("Computer chose ", comp)
     print("You choose ", user_Turn)
 
     if function_returning == None:
-        playsound("my recordings\\The game is tie.mpeg")
         print("\n\t\tThe game is a Tie.(Draw)\n")
+        playsound("my recordings\\The game is tie.mpeg")
     elif function_returning == True:
-        playsound("my recordings\\User won the  game .mpeg")
         print("\n\t\tYay!! You won the game.\n")
+        playsound("my recordings\\User won the  game .mpeg")
     elif function_returning == False:
-        playsound("my recordings\\computer wins.mpeg")
         print("\n\t\tSorry, you lost the game. Better Luck next time.\n")
+        playsound("my recordings\\computer wins.mpeg")
     
     choice = input("\nDo you want to play the game again (y/n): ")
